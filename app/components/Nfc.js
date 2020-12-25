a16fc104a9619cd45bcf7e5ff37b42103d90a410
Faith Culas <faithculas@gmail.com>
2020-12-25 14:14:18 +0530

    finalized NFC posting

--- app/components/Nfc.js
+++ app/components/Nfc.js
@@ -18,9 +18,46 @@ import NfcManager, {
   NdefParser,
 } from 'react-native-nfc-manager';
 
-import ListItem from '../components/ListItem';
+import ListItem from './ListItem';
+import MyIcon from './MyIcon'
 
 class Nfc extends React.Component {
+  getDataUsingPost = (inf) => {
+    //console.log(inf);
+    //POST json
+    var dataToSend = inf;
+    //var dataToSend = {date: '2020-10-22', time: '12-15-00', temp: 20, userid: 1, isid: '5f8bdbea7119bc007641a5c4'};
+    //making data to send on server
+    var formBody = [];
+    for (var key in dataToSend) {
+      var encodedKey = encodeURIComponent(key);
+      var encodedValue = encodeURIComponent(dataToSend[key]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    formBody = formBody.join('&');
+    console.log(formBody);
+    //POST request
+    fetch('https://snapit-api.herokuapp.com/api/setlog', {
+      method: 'POST', //Request Type
+      body: formBody, //post body
+      headers: {
+        //Header Defination
+        'Content-Type': 
+          'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+    })
+    .then((response) => response.json())
+    //If response is in json then in success
+    .then((responseJson) => {
+      alert(JSON.stringify(responseJson));
+      console.log(responseJson);
+    })
+    //If response is not in json then in error
+    .catch((error) => {
+      alert(JSON.stringify(error));
+      console.error(error);
+    });
+  };
   
   componentDidMount(){
     const id =this.props.id
@@ -70,8 +107,14 @@ class Nfc extends React.Component {
     NfcManager.unregisterTagEvent().catch(() => 0);
 
     // Decrypt the NFC data
+    // lahiru change here what you want
     this.content = this.state.parsed[0];
     console.log('Content from read:', content, "id passed:", this.props.id)
+    const values = {date: this.props.id[1], time: this.props.id[2], temp:this.content, userid: 1, isid: '5f8bdbea7119bc007641a5c4'}
+    this.getDataUsingPost(values);
+    Alert.alert("scanned");
+
+    // end here
 
     var endOfIV = content.indexOf(' ~ ')
     var iv = content.substring(0,endOfIV).trim()
@@ -85,8 +128,10 @@ class Nfc extends React.Component {
 
   test = () => {
     console.log(this.props.id);
+    const values = {date: this.props.id[1], time: this.props.id[2], temp: 20, userid: 1, isid: '5f8bdbea7119bc007641a5c4'}
+    this.getDataUsingPost(values);
     Alert.alert("scanned");
-    
+
 
   }
  
@@ -94,7 +139,7 @@ render(){
   return (
     <SafeAreaView style={{padding: 20}}>
       <Text>NFC SCANNER</Text>
-      <Button title="Scanner" onPress={this.test}></Button>
+      <ListItem onPress={this.readData} icon={<MyIcon name= "scan" size={80} backColor="#f4f4f2" iconColor="black"/>}/>
 
     </SafeAreaView>
   );
