4245802ed98a432484d3a7ac6cf4724b061940c2
Faith Culas <faithculas@gmail.com>
2020-12-26 15:47:25 +0530

    implemented authentication

--- App.js
+++ App.js
@@ -14,32 +14,98 @@ import {
   View,
   Text,
   StatusBar,
+  ImageBackground,
+  Alert
 } from 'react-native';
-
+import { useState } from 'react';
 
 import Icon from 'react-native-vector-icons/Ionicons'
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import Auth0 from 'react-native-auth0';
 
 import ProfileScreen from './app/Screens/ProfileScreen';
 import GraphScreen from './app/Screens/GraphScreen';
 import AccountScreen from './app/Screens/AccountScreen';
 import Screen from './app/components/Screen';
-
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
-import AuthNavigator from './app/navigation/AuthNavigator'
+import AuthNavigator from './app/navigation/AuthNavigator';
+import LoginScreen from './app/Screens/LoginScreen';
+import WelcomeScreen from './app/Screens/WelcomeScreen';
+import LoginButton from './app/components/LoginButton'
+
 
 const App: () => React$Node = () => {
+const[access,setAccess]=useState(false);
+
+const auth0 = new Auth0(
+  { domain: 'dev-wxf2dldy.us.auth0.com', 
+  clientId: 'enKTnDziZOeOZomNcSHXwehfxymZl0aY' });
+
+const Stack=createStackNavigator();
+
+function Login(){
+  auth0
+        .webAuth
+        .authorize({scope: 'openid profile email'})
+        .then(credentials =>{
+            Alert.alert(
+                'Logged in!'
+            );
+            setAccess(true);
+        })
+        .catch(error => console.log(error));
+        //console.log("hello")
+
+}
   return (
-    <>
+    access ? 
+    (<>
       <StatusBar barStyle="dark-content" />
       <NavigationContainer theme={navigationTheme}>
-          <AppNavigator/>
+          <AppNavigator />
       </NavigationContainer>
        
-    </>
+    </>):
+    <>
+    <Screen>
+      <ImageBackground
+        style={styles.background}
+        source={require('./app/assets/background.jpg')}>
+      <View style={styles.logoContainer}>
+      <Text style={styles.tagline}>SnapIT</Text>
+      </View>
+      {/* <LoginButton title="Login" onPress={}></LoginButton> */}
+        <LoginButton title="Login" onPress={Login} />
+      </ImageBackground>
+    </Screen>   
+  </>
   );
 };
-
+const styles = StyleSheet.create({
+  background:{
+      flex:1,
+      justifyContent:"flex-end",
+      alignItems:"center",
+      padding:"10%"
+  },
+  logo:{
+      width:100,
+      height:100,
+  },
+  logoContainer:{
+      position:'absolute',
+      top:70,
+      alignItems:"center"
+  },
+  tagline:{
+      fontFamily:Platform.OS==="android"?"Roboto":"Avenir",
+      fontSize:30,
+      fontWeight:"bold",
+      fontStyle:"italic",
+      color:"white",
+  }
+})
 export default App;
--- app/Screens/LoginScreen.js
+++ app/Screens/LoginScreen.js
@@ -1,15 +1,60 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React,{useState} from 'react';
+import { Text,Alert } from 'react-native';
+import Auth0 from 'react-native-auth0';
 
 import Screen from '../components/Screen';
+import LoginButton from '../components/LoginButton'
 
-function LoginScreen(props) {
-    return (
-        <Screen>
-            <Text>Login here</Text>
-        </Screen>
+const auth0 = new Auth0(
+    { domain: 'dev-wxf2dldy.us.auth0.com', 
+    clientId: 'enKTnDziZOeOZomNcSHXwehfxymZl0aY' });
+
+class LoginScreen extends React.Component {
+    constructor() {
+        super();
+        this.state = { accessToken: false };
         
-    );
+      };
+
+        
+    Login =()=>{
+        auth0
+        .webAuth
+        .authorize({scope: 'openid profile email'})
+        .then(credentials =>{
+            Alert.alert(
+                'Logged in!'
+            );
+            
+            //this.setState({ accessToken: credentials.accessToken });
+            this.setState({
+                accessToken: true
+              });
+            console.log(this.state.accessToken)
+        }
+          // Successfully authenticated
+          // Store the accessToken
+          
+        )
+        .catch(error => console.log(error));
+        //console.log("hello")
+    }
+
+
+        render (){
+            return(<>
+                <LoginButton title="Login" onPress={this.Login} />
+                </>)
+        }
 }
+
+// LoginScreen.abc = function(){
+//     console.log("hello")
+    
+// }
+// module.exports = {
+//   functions: LoginScreen
+// };
+
 
 export default LoginScreen;
